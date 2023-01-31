78de475b27e603a4b8d8a44343232a88cf0cbc4b
Yan Dias <yansantos0712@gmail.com>
2023-01-31 16:07:38 -0300

    feat:complete challenge

--- src/pages/Home/components/Featured/index.tsx
+++ src/pages/Home/components/Featured/index.tsx
@@ -3,7 +3,7 @@ import { formatDistance } from "date-fns";
 import { api } from "../../../../services/api";
 import { Container, LinkArticle, SectionList } from "./styles";
 import ptBR from "date-fns/esm/locale/pt-BR";
-import { Link } from "react-router-dom";
+import { useForm } from "react-hook-form";
 
 interface PublicationData {
   body: string;
@@ -12,8 +12,13 @@ interface PublicationData {
   number: number;
 }
 
+interface SearchFormQuery {
+  query: string;
+}
+
 export function Featured() {
   const [publications, setPublications] = useState<PublicationData[]>([]);
+  const { register, handleSubmit, reset } = useForm<SearchFormQuery>();
 
   const getPublicationsInGithub = useCallback(async () => {
     const { data } = await api.get(
@@ -21,6 +26,14 @@ export function Featured() {
     );
     setPublications(data);
   }, []);
+
+  async function handleSearchPublication({ query }: SearchFormQuery) {
+    const { data } = await api.get(
+      `search/issues?q=${query}%20repo:rocketseat-education/reactjs-github-blog-challenge`
+    );
+    setPublications(data.items);
+    reset();
+  }
 
   useEffect(() => {
     getPublicationsInGithub();
@@ -32,33 +45,39 @@ export function Featured() {
         <strong>Publicações</strong>
         <span>{publications.length} publicações</span>
       </header>
-      <form>
-        <input type="text" placeholder="Buscar conteúdo" />
+      <form onSubmit={handleSubmit(handleSearchPublication)}>
+        <input {...register("query")} placeholder="Buscar conteúdo" />
       </form>
       <SectionList>
-        {publications.map((publication) => (
-          <LinkArticle
-            to={`/${publication.number ?? "0"}`}
-            key={publication.number}
-          >
-            <article>
-              <header>
-                <strong>{publication.title}</strong>
-                <time>
-                  {formatDistance(
-                    new Date(),
-                    new Date(publication.created_at),
-                    {
-                      addSuffix: true,
-                      locale: ptBR,
-                    }
-                  )}
-                </time>
-              </header>
-              <p>{publication.body.slice(0, 200)}</p>
-            </article>
-          </LinkArticle>
-        ))}
+        {publications.length > 0 ? (
+          publications.map((publication) => (
+            <LinkArticle
+              to={`/${publication.number ?? null}`}
+              key={publication.number}
+            >
+              <article>
+                <header>
+                  <strong>{publication.title}</strong>
+                  <time>
+                    {formatDistance(
+                      new Date(),
+                      new Date(publication.created_at),
+                      {
+                        addSuffix: true,
+                        locale: ptBR,
+                      }
+                    )}
+                  </time>
+                </header>
+                <p>{publication.body.slice(0, 200)}</p>
+              </article>
+            </LinkArticle>
+          ))
+        ) : (
+          <header>
+            <strong>Nenhum resultado encontrado</strong>
+          </header>
+        )}
       </SectionList>
     </Container>
   );
